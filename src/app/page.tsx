feb13552bcb846cded08c5ffe3dfa2b2749f152b
Mark <koshkinmark@gmail.com>
2025-04-13 18:30:26 -0400

    feat: added export script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <Box className={styles.page}>
-      <Typography variant="h1">      Oy Oy are are
+      <Typography variant="h1"> Weee hee
       </Typography>
 
-      <Link href="/chords-table">Chords Table</Link>
+
+      <Card>
+        <Link href="/chords-table">Chords Table</Link>
+      </Card>
     </Box>
   );
 }
