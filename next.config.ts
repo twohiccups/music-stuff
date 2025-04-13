60a1834dd26d202dbec0f66ccbe5ce1267b5cf51
Mark <koshkinmark@gmail.com>
2025-04-13 18:18:04 -0400

    feat: github pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
