80d9964c8a54ad1ab49fffa755913339cccc982a
Mark <koshkinmark@gmail.com>
2025-04-13 19:27:57 -0400

    feat: basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: "/music-stuff",
   images: {
     unoptimized: true,
   },
